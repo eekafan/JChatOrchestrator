d506c3bdf6e92b3406030271d2ef254eca81b088
Andrew Rombach <andrew.rombach@uk.ibm.com>
2018-08-07 15:41:30 +0100

    Fixes to LDAPConfig

--- JChatOrchestrator/src/com/rombachuk/jchatorchestrator/LDAPConfig.java
+++ JChatOrchestrator/src/com/rombachuk/jchatorchestrator/LDAPConfig.java
@@ -1,10 +1,5 @@
 package com.rombachuk.jchatorchestrator;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import javax.servlet.ServletContext;
 
 public class LDAPConfig {
