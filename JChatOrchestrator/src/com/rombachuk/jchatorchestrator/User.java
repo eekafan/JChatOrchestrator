be3283856f553fd0ee4a677f9a9e7bbfd389d30b
Andrew Rombach <andrew.rombach@uk.ibm.com>
2018-08-07 11:38:18 +0100

    Initial servlet build

---
+++ JChatOrchestrator/src/com/rombachuk/jchatorchestrator/User.java
@@ -0,0 +1,82 @@
+package com.rombachuk.jchatorchestrator;
+
+import java.util.List;
+
+import com.unboundid.ldap.sdk.BindRequest;
+import com.unboundid.ldap.sdk.BindResult;
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldap.sdk.LDAPSearchException;
+import com.unboundid.ldap.sdk.ResultCode;
+import com.unboundid.ldap.sdk.SearchResult;
+import com.unboundid.ldap.sdk.SearchResultEntry;
+import com.unboundid.ldap.sdk.SearchScope;
+import com.unboundid.ldap.sdk.SimpleBindRequest;
+
+public class User {
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDn() {
+		return dn;
+	}
+
+	public void setDn(String dn) {
+		this.dn = dn;
+	}
+
+	public List<String> getRoles() {
+		return Roles;
+	}
+
+	public void setRoles(List<String> roles) {
+		Roles = roles;
+	}
+
+	String name;
+	String dn;
+	List<String> Roles;	
+	
+	public User(LDAPConfig ldapconfig, String username) throws LDAPException, LDAPSearchException {
+		this.name = username;
+		this.dn = "notfound";
+	  
+		LDAPConnection ldapconnection = new LDAPConnection(ldapconfig.getHostname(),
+                ldapconfig.getHostport(),ldapconfig.getBinddn(),ldapconfig.getBindpassword());
+	   	if (ldapconnection.isConnected()) {
+     	 String userFilter = "(&"+ldapconfig.getUserclassfilter()+
+     			            "("+ldapconfig.getUseruniqueattribute()+"="+username+"))";
+     	 SearchResult searchResult = ldapconnection.search(ldapconfig.getUserbasedn(), 
+     			                    SearchScope.SUB, userFilter);
+     	 List<SearchResultEntry> results = searchResult.getSearchEntries();
+       	 if (!results.isEmpty()) {
+    		this.dn = results.get(0).getDN();
+    	  } 
+       	 ldapconnection.close();
+	   	}
+	 }
+	   	
+	 public static Boolean authenticate(LDAPConfig ldapconfig, String userdn, String credentials) throws LDAPException {
+		 
+		    Boolean authenticated = false;
+			LDAPConnection ldapconnection = new LDAPConnection(ldapconfig.getHostname(),
+                          ldapconfig.getHostport(),ldapconfig.getBinddn(),ldapconfig.getBindpassword());
+		 	if (ldapconnection.isConnected()) {
+				BindRequest bindRequest = new SimpleBindRequest(userdn,credentials);
+		    	BindResult bindResult = ldapconnection.bind(bindRequest);
+		    	if(bindResult.getResultCode().equals(ResultCode.SUCCESS)) {
+		    		authenticated = true;
+		    	}
+		        ldapconnection.close();
+		 	}
+			return authenticated;			
+	}
+	
+
+
+}
---
+++ JChatOrchestrator/src/com/rombachuk/jchatorchestrator/loginServlet.java
@@ -0,0 +1,82 @@
+package com.rombachuk.jchatorchestrator;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldap.sdk.LDAPSearchException;
+
+
+/**
+ * Servlet implementation class loginServlet
+ */
+@WebServlet("/loginServlet")
+public class loginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public loginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    response.setContentType("text/html");  
+	    PrintWriter out = response.getWriter();  
+	          
+        String destination = "relogin.html"; 
+        
+        try {
+    
+	     LDAPConfig ldapconfig = new LDAPConfig(request.getServletContext());        
+         User       user = new User(ldapconfig,request.getParameter("username"));
+
+         if (user.getDn().equals("notfound")) {
+        		destination = "relogin.html";
+         } 
+         else {
+        	 	Boolean testCredentials = User.authenticate(ldapconfig, user.getDn(), request.getParameter("userpass"));
+                if (testCredentials == true) {
+                	destination = "success.html";
+                }
+                else {
+                	destination = "relogin.html";
+                }
+         }
+        }
+        catch (LDAPSearchException e) {
+        	
+        }
+        catch (LDAPException e) {
+        	
+        }
+        
+ 
+     	if (destination.equals("relogin.html")){ 
+       			HttpSession session = request.getSession();
+       			session.invalidate();
+       	}
+       	response.sendRedirect(destination);
+	    out.close();  
+    }
+}
